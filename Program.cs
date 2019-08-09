e27603af2a4f7d59ce7203f09311ebdca6029aeb
Sean Gillespie <seangillespie@Seans-MacBook-Pro.local>
2019-08-09 08:22:32 -0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Deck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Card
+    {
+        public string stringVal
+        {
+
+        }
+    }
+
+    class Deck
+    {
+        
+    }
+}
